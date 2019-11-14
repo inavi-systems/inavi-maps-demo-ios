364c8cbd72688139c83d6fa70a1573d4cbbbecba
csk46 <csk0406@gmail.com>
2019-11-14 14:32:37 +0900

    Demo 0.2.0

---
+++ App/Source/ViewController/LocationIconViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+import iNaviMaps
+
+class LocationIconViewController: MapViewController {
+
+    @IBOutlet weak var locationIconImageSwitch: UISwitch!
+    @IBOutlet weak var locationIconScaleSwitch: UISwitch!
+    @IBOutlet weak var locationIconCircleRadiusSwitch: UISwitch!
+    @IBOutlet weak var locationIconCircleColorSwitch: UISwitch!
+    @IBOutlet weak var locationIconBearingLabel: UILabel!
+    @IBOutlet weak var locationIconBearingSlider: UISlider!
+    @IBOutlet weak var locationIconBearingValueLabel: UILabel!
+    
+    var locationIcon: INVLocationIcon? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationIcon = mapView.locationIcon
+        
+        locationIcon?.position = mapView.cameraPosition.target
+        locationIcon?.touchEvent = { (shape) in
+            let alert = UIAlertController(title: "위치 아이콘 클릭됨",
+                                          message: nil,
+                                          preferredStyle: .alert)
+            self.present(alert, animated: true, completion: {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5, execute: {
+                    alert.dismiss(animated: true, completion: nil)
+                })
+            })
+            return true
+        }
+    }
+    
+    func didTapMapView(_ point: CGPoint, latLng latlng: INVLatLng) {
+        locationIcon?.position = latlng
+    }
+    
+    @IBAction func respondToLocationIconVisible(_ sender: UISwitch) {
+        locationIcon?.isVisible = sender.isOn
+        
+        locationIconImageSwitch.isEnabled = sender.isOn
+        locationIconScaleSwitch.isEnabled = sender.isOn
+        locationIconCircleRadiusSwitch.isEnabled = sender.isOn
+        locationIconCircleColorSwitch.isEnabled = sender.isOn
+        locationIconBearingLabel.isEnabled = sender.isOn
+        locationIconBearingSlider.isEnabled = sender.isOn
+        locationIconBearingValueLabel.isEnabled = sender.isOn
+    }
+    
+    @IBAction func respondToLocationIconImage(_ sender: UISwitch) {
+        locationIcon?.image = sender.isOn ? INVImage(name: "baseline_directions_run_black_36pt") : INV_LOCATION_ICON_IMAGE
+    }
+    
+    @IBAction func respondToLocationIconScale(_ sender: UISwitch) {
+        locationIcon?.scale = sender.isOn ? 2.0 : 1.0
+    }
+    
+    @IBAction func respondToLocationIconCircleRadius(_ sender: UISwitch) {
+        locationIcon?.circleRadius = sender.isOn ? 48.0 : 24.0
+    }
+    
+    @IBAction func respondToLocationIconCircleColor(_ sender: UISwitch) {
+        locationIcon?.circleColor = sender.isOn ? UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 0.5) : INV_LOCATION_ICON_CIRCLE_COLOR
+    }
+    
+    @IBAction func respondToLocationIconBearing(_ sender: UISlider) {
+        locationIcon?.bearing = CGFloat(sender.value)
+        locationIconBearingValueLabel.text = "\(Int(sender.value))°"
+    }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Changelog for the iNavi Maps SDK for iOS
+
+## 0.2.0 - 2019-11-14
+
+### Features
+ - `LocationIcon` 기능 추가 (위치 아이콘의 모양과 위치를 변경할 수 있는 기능)
+
+### Bug fixes
+ - 특정 지도 레벨에서 셰이프가 사라지는 오류 수정
+ - 간헐적으로 축적바의 거리가 비정상 표출하는 오류 수정
+ 
+### Improvements
+ - 빠르게 지도 레벨 변경 시 지도 아이콘이 겹치는 현상 개선
+
+## 0.1.0 - 2019-10-29
+- iOS SDK 초기 배포
